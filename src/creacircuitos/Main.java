5a890d306c452ab7c04f852ec0de15c487eb13ca
ankore <ankorecpy@outlook.com>
2018-05-10 18:15:40 -0500

    Primer Commit, El proyecto inicia de cero

---
+++ src/creacircuitos/Main.java
@@ -0,0 +1,12 @@
+package creacircuitos;
+
+/**
+ * @author Alejandro Méndez Astudillo
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
